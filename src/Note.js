30a01b0ca96d88b84aaada92d624937caa68b195
Ishan <ishanliou@yahoo.com>
2018-05-25 17:46:02 -0700

    save btn

--- src/Note.js
+++ src/Note.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react'
 import FaPencil from 'react-icons/lib/fa/pencil'
 import FaTrash from 'react-icons/lib/fa/trash'
+import FaFloppyO from 'react-icons/lib/fa/floppy-o'
 
 
 class Note extends Component {
     constructor(props){
         super(props)
+        this.state = {
+            editing: false
+        }
         this.edit = this.edit.bind(this)
         this.remove = this.remove.bind(this)
+        this.save = this.save.bind(this)
+        this.renderForm = this.renderForm.bind(this)
+        this.renderDisply = this.renderDisply.bind(this)
     }
+
     edit() {
-        alert('editing note')
+        this.setState({
+            editing: true
+        })
     }
     remove() {
         alert('removing note')
     }
-    render() {
+    save() {
+        alert('saved!')
+    }
+    renderForm(){
+        return (
+            <div className="note">
+            <form>
+                <textarea />
+                <button onClick={this.save}><FaFloppyO /></button>
+            </form>
+
+            </div>
+        )
+    }
+    renderDisply() {
         return(
 
             <div className="note">
@@ -28,6 +52,13 @@ class Note extends Component {
             </div>
 
         )
+    }
+    render(){
+        if(this.state.editing) {
+            return this.renderForm()
+        } else {
+            return this.renderDisply()
+        }
     }
 }
 
