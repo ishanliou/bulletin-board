2d4025582e70d305f2c4215f2f509ae72fd7fdd8
Ishan <ishanliou@yahoo.com>
2018-05-29 16:37:01 -0700

    add btn

--- src/Board.js
+++ src/Board.js
@@ -1,31 +1,37 @@
 import React, { Component } from 'react'
 import Note from './Note'
+import FaPlus from 'react-icons/lib/fa/plus'
 
 class Board extends Component {
     constructor(props){
         super(props)
         this.state = {
-            notes: [
-                {
-                    id: 0,
-                    note: "Call Lisa"
-                },
-                {
-                    id: 1,
-                    note: "Email John"
-                },
-                {
-                    id: 2,
-                    note: "Order printer ink"
-                }
-            ]
+            notes: []
         }
+        this.add = this.add.bind(this)
         this.eachNote = this.eachNote.bind(this)
         this.update = this.update.bind(this)
         this.remove = this.remove.bind(this)
+        this.nextId = this.nextId.bind(this)
     }
 
-update(newText, i){
+add (text) {
+    this.setState(prevState => ({
+        notes: [
+            ...prevState.notes,
+            {
+                id: this.nextId(),
+                note: text
+            }
+        ]
+    }))
+}
+
+nextId() {
+    this.uniqueId = this.uniqueId || 0
+    return this.uniqueId++
+}
+update(newText, i) {
     console.log("updating item at index", i, newText)
     this.setState(prevState => ({
         notes: prevState.notes.map(
@@ -36,14 +42,14 @@ update(newText, i){
     }))
 }
 
-remove(id){
+remove(id) {
     console.log("removing item at", id)
     this.setState(prevState =>({
         notes: prevState.notes.filter(note => note.id !==id)
     }))
 }
 
-eachNote (note, i) {
+eachNote(note, i) {
     return (
         <Note key={i}
                 index={i}
@@ -58,6 +64,10 @@ render() {
     return (
         <div className="board">
             {this.state.notes.map(this.eachNote)}
+            <button onClick={this.add.bind(null, "New Note")}
+                    id="add">
+                <FaPlus />
+            </button>
         </div>
     )
 }
