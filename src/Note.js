64527df9cf0e62105f82cda6c54be261bfba91db
Ishan <ishanliou@yahoo.com>
2018-05-29 23:11:56 -0700

    random display notes

--- src/Note.js
+++ src/Note.js
@@ -15,6 +15,19 @@ class Note extends Component {
         this.save = this.save.bind(this)
         this.renderForm = this.renderForm.bind(this)
         this.renderDisply = this.renderDisply.bind(this)
+        this.randomBetween =this.randomBetween.bind(this)
+    }
+
+    componentWillMount() {
+        this.style = {
+            right: this.randomBetween(0, window.innerWidth - 150, 'px'),
+            top: this.randomBetween(0, window.innerHeight -150, 'px'),
+            transform: `rotate(${this.randomBetween(-25, 25, 'deg')})`
+        }
+    }
+
+    randomBetween(x, y, s) {
+        return x + Math.ceil(Math.random() * (y-x)) + s
     }
 
     edit() {
@@ -38,7 +51,7 @@ class Note extends Component {
     
     renderForm(){
         return (
-            <div className="note">
+            <div className="note" style={this.style}>
             <form onSubmit={this.save}>
                 <textarea ref={input => this._newText = input}/>
                 <button id="save"><FaFloppyO /></button>
@@ -50,7 +63,7 @@ class Note extends Component {
     renderDisply() {
         return(
 
-            <div className="note">
+            <div className="note" style={this.style}>
                 <p>{this.props.children}</p>
                 <span>
                     <button onClick={this.edit} id="edit"><FaPencil /></button>
